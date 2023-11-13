db389bdaf55f9737d52ccadc7bc90d8c97dab97d
LuisPulenta <luisalbertonu@gmail.com>
2023-11-13 11:26:45 -0300

    Video 718

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -8,6 +8,10 @@ class DashboardController {
 
     public static function index(Router $router) {
 
+        if(!is_admin()){
+            header('Location:/login');
+        }
+        
          // Render a la vista 
          $router->render('admin/dashboard/index', [
             'titulo' => 'Panel de Administración'
